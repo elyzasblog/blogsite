0c7b0ead743e2ec5203ce28c0b2b46e28658e060
Lemuel Untalan <untalanlem@gmail.com>
2021-05-09 00:40:36 +0800

    Worked on the Home page banner, TLD Section, About Me Section'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 $('document').ready(function(){
     $('#overlayWhite').addClass('animate__fadeOut');
+    
+    setTimeout(function(){ 
+      $('#overlayWhite').remove();
+    }, 2000);
+
+    $('#sayHi').on('mouseover', function(){
+      $('#sayHi').addClass('show active');
+      $('#sayHiDropdown').addClass('show');
+    });
+
+    $('#sayHiDropdown').on('mouseleave', function(){
+      $('#sayHi').removeClass('show active');
+      $('#sayHiDropdown').removeClass('show');
+    })
 });
