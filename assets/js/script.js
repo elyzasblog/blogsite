417cfdb5b9e6dcadda4a3afcdfe1e1c6a18d2655
Lemuel Untalan <untalanlem@gmail.com>
2021-07-03 01:11:18 +0800

    Almost finish the home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 $('document').ready(function(){
     $('#overlayWhite').addClass('animate__fadeOut');
     
@@ -14,4 +16,43 @@ $('document').ready(function(){
       $('#sayHi').removeClass('show active');
       $('#sayHiDropdown').removeClass('show');
     })
+
+    $('.featured-articles').remove();
+
+    $('.back-to-top').on('click', function(){
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    })
 });
+
+window.onscroll = function() { navbarScroll(); backToTopScroll(); };
+
+var header = document.querySelector(".main-header");
+
+var sticky = header.offsetTop;
+
+function backToTopScroll() {
+  if (window.pageYOffset > sticky) {
+    $('.back-to-top').removeClass('d-none')
+  } else {
+    $('.back-to-top').addClass('d-none')
+  }
+}
+
+function navbarScroll() {
+  if ( window.innerWidth <= 768 ) { return; }
+  if (window.pageYOffset > sticky) {
+    $('.main-header')
+    .css({
+      'background-color': '#1E1C1C', 
+      'transition': 'background-color 500ms'
+    })
+    .addClass('scrolled')
+  } else {
+    $('.main-header').css({
+      'background-color': 'transparent', 
+      'transition': 'background-color 500ms'
+    })
+    .removeClass('scrolled')
+  }
+}
